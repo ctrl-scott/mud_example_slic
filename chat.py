9a97709028cd7ed56ed7bfe3fed3278f3267f398
ctrl-scott <oscott66@outlook.com>
2025-08-04 10:11:04 -0500

    adding options

--- chat.py
+++ chat.py
@@ -9,8 +9,12 @@ def chat_encounter(player):
         "Tell me one historical event that shaped your love life."
     ]
     print("They say:", random.choice(responses))
-    action = input("Respond with /flirt, /truth, or /block: ")
+    action = input("Respond with /flirt, /truth, /lifesaver or /block: ")
 
+#added optional /lifesaver has to be further integrated
+    if action == "/lifesaver":
+            print("You activate a lifesaver (You only get 3 in the game, infinite lifesavers for \n for helping others.)")
+            player.reputation -= 1
     if action == "/flirt":
         print("They flirt back. You gain +5 reputation.")
         player.reputation += 5
